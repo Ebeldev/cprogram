6d891e4659c4ba5814b740f1550b88e3c004ad1d
Ebelanger <ebelanger@hotmail.com>
2015-02-27 00:33:40 -0500

    upadate

---
+++ gitcommands.c
@@ -0,0 +1,9 @@
+// Commads for GitHub
+
+: git clone http://www....... // to copy a repository to a computer
+: git status  // to know if the status of the folder is ok
+: git add -A// add the file to the queue
+: git commit -m"added this new files" //
+: git push // to synch all files to github.com
+: git pull // take content from the github.com to computer
+
